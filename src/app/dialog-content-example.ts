2b894ef55a844a9c980d88d7baf5a92a775e5f6e
wasim117 <wasim117@gmail.com>
2022-05-10 12:08:19 +0530

    files added

--- src/app/dialog-content-example.ts
+++ src/app/dialog-content-example.ts
@@ -1,5 +1,5 @@
-import {Component} from '@angular/core';
-import {MatDialog} from '@angular/material/dialog';
+import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 
 /**
  * @title Dialog with header, scrollable content and actions
@@ -12,9 +12,18 @@ export class DialogContentExample {
   constructor(public dialog: MatDialog) {}
 
   openDialog() {
-    const dialogRef = this.dialog.open(DialogContentExampleDialog);
+    const dialogRef = this.dialog.open(DialogContentExampleDialog, {
+      panelClass: 'mytheme-dialog',
+      backdropClass: ['black-60', 'white-60'],
+      minHeight: '100px',
+      height: '80vh',
+      maxHeight: '600px',
+      width: '90%',
+      minWidth: '100px',
+      maxWidth: '1200px',
+    });
 
-    dialogRef.afterClosed().subscribe(result => {
+    dialogRef.afterClosed().subscribe((result) => {
       console.log(`Dialog result: ${result}`);
     });
   }
@@ -25,7 +34,6 @@ export class DialogContentExample {
   templateUrl: 'dialog-content-example-dialog.html',
 })
 export class DialogContentExampleDialog {}
-
 
 /**  Copyright 2022 Google LLC. All Rights Reserved.
     Use of this source code is governed by an MIT-style license that
